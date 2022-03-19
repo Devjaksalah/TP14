8ab46ed1620b3df32551bb31c7c7ee700d00be23
Devjaksalah <jakanesalah@gmail.com>
2022-03-19 23:57:27 +0100

    TP14

--- src/main/java/ma/cigma/FrontEndApplication.java
+++ src/main/java/ma/cigma/FrontEndApplication.java
@@ -1,13 +1,16 @@
 package ma.cigma;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@ComponentScan("{ma.cigma}")
+
 public class FrontEndApplication {
 
 	public static void main(String[] args) {
@@ -18,6 +21,13 @@ public class FrontEndApplication {
 	// kayt3f fga3 les class okadir lih autowird
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+
+
 	}
+	@Bean
+	PasswordEncoder getPass(){
+		return  new BCryptPasswordEncoder();
+	}
+
 
 }
